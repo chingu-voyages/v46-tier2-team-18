a83745f44178768e1c872c89ee0380daba972978
futurefounder <jsk@posteo.de>
2023-10-30 10:11:49 +0100

    Fixed dom errors

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Main from './components/Main';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div class="min-h-screen">
+    <div className="min-h-screen">
       <Header />
       <Main />
       <Footer />
